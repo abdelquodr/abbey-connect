2f5497de093b7e49610ab5ea22b978791bd83456
Abdelquodr Salaudeen <a-salaudeen@leadway.com>
2026-05-24 09:47:10 +0100

    Ignore temp files and remove tracked artifacts

--- backend/.tmp/yarn--1779577858351-0.9710823168073739/node.cmd
+++
@@ -1,1 +0,0 @@
-@"C:\Users\a-salaudeen\tools\node\v24.4.1\node-v24.4.1-win-x64\node.exe"   %*
--- backend/.tmp/yarn--1779606369242-0.8990091259022059/yarn.cmd
+++
@@ -1,1 +0,0 @@
-@"C:\Users\a-salaudeen\tools\node\v24.4.1\node-v24.4.1-win-x64\node.exe"  "C:\Users\a-salaudeen\AppData\Roaming\npm\node_modules\yarn\bin\yarn.js"  %*
